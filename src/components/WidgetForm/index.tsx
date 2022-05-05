15b24f27dd900eb96640ce02edb703aaa1e69fb8
Felipe Wandork <wandork@gmail.com>
2022-05-05 13:10:38 -0300

    Finalização da Aula 02

---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,74 @@
+import { CloseButton } from "../CloseButton";
+import bug from "../../assets/bug.svg";
+import idea from "../../assets/idea.svg";
+import thought from "../../assets/thought.svg";
+import { useState } from "react";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
+
+export const feedbackTypes = {
+
+    BUG: {
+        title: "Problema",
+        image: {
+            source: bug,
+            alt: "Imagem de um inseto"
+        }
+    },
+    IDEA: {
+        title: "Ideia",
+        image: {
+            source: idea,
+            alt: "Imagem de uma lâmpada"
+        }
+
+    },
+    OTHER: {
+        title: "Outro",
+        image: {
+            source: thought,
+            alt: "Imagem de um balão de pensamento"
+        }
+
+    },
+
+}
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+
+    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+    const [feedbackSent, setFeedbackSent] = useState(false);
+
+    function handleRestartFeedback(){
+        setFeedbackSent(false);
+        setFeedbackType(null);
+    }
+
+    return (
+        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+
+            { feedbackSent ? (
+                <FeedbackSuccessStep onFeedbackRestartRequested={handleRestartFeedback} />
+            ) : (
+            <>
+            {!feedbackType ? (
+                <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+            ) : (
+                <FeedbackContentStep 
+                    feedbackType={feedbackType} 
+                    onFeedbackRestartRequested={handleRestartFeedback}
+                    onFeedbackSent={()=>setFeedbackSent(true)}
+                />
+            )}
+            </>
+            )}
+
+            <footer className="text-xs text-neutral-400">
+                Feito com ♥ por <a className="underline underline-offset-4" href="https://www.instagram.com/wandork">Felipe Wandork</a>
+            </footer>
+        </div>
+    );
+}
